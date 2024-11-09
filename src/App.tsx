6ab518907d79cb35ddd634935e8d933f455b4322
Cor Baars <corbaars@gmail.com>
2024-11-09 18:18:17 +0100

    Faqs 1

--- src/App.tsx
+++ src/App.tsx
@@ -73,13 +73,33 @@ const App: React.FC = (): ReactElement => {
               
                 <details>
                   <summary>Waarom is MyContext veilig?</summary>
-                  <p>Hier komt het antwoord over de veiligheid van MyContext...</p>
+                  <p>MyContexts werkt zonder servers. 
+                    Servers die kunnen worden gehackt waardoor je gegevens op straat komen te liggen.
+                    Servers van bedrijven die aan de haal gaan met je gegevens en er veel aan verdienen.
+                    Geen Server geen problemen met je privacy </p>
                 </details>
                 <details>
-                  <summary>noot</summary>
-                  <p>Hier komt de uitleg over de noot...</p>
+                  <summary>Hoe werkt MyContexts?</summary>
+                  <p>MyContexts applicaties worden op een speciale manier gemaakt. 
+                    Bij MyContexts hoort een taal die in termen van Contexten, Rollen en Perspectieven
+                    (Gebruikerrollen hebben Perspectieven op andere Gebruikerrollen en 
+                    Dingrollen) een toepassing vormgeeft. 
+                    Op basis hiervan rekenen we uit welke gebruikers in een Context
+                    welke gegevens moet krijgen om zijn of haar rol in een Context te spelen. 
+                    Vervolgens sturen we die gegevens rechtstreeks naar de devices van die gebruikers. 
+                    Dus niet via een Server.</p>
                 </details>
-              
+                <details>
+                  <summary>Wat is het verschil met bijvoorbeeld WhatApp?</summary>
+                  <p>Om te beginnen is MyContext niet een App, maar een taal en een platform.
+                  In WhatsApp draat het om Chats. 
+                  Iedere Contexts in MyContexts heeft standaard een Chat maar ook gegevens en andere Contexten waar je heen kan gaan. 
+                  We noemen dit de Wie-Wat-Waar opzet. Wie doen er allemaal mee in een Context, 
+                  Wat voor gegevens zijn er in de Context waar je samen aan kuntwerken en 
+                  Waar kun je naar allemaal naar toe. 
+                  We denken dan ook dat WWW binnenkort niet meer staat voor World Wide Web maar voor Wie Wat Waar.
+                  </p>
+                </details>
             </ul>
           </div>
         </Panel>
